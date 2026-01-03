251510fefebd4c43d3fc07cb321e7b33dbcd4ec2
original6001 <lukas.cols@icloud.com>
2026-01-03 17:55:09 +0000

    Added a "Start on" for the autoclicker.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 clicks = 0
+recorded_key = ""
 clickstop = False
 
 root = tk.Tk()
@@ -32,6 +33,22 @@ style.configure("TCombobox", background="#1f1732", foreground="black")
 
 clickspeed = tk.StringVar(root)
 clickspeed.set("10 CPS")
+
+
+def record():
+    global recorded_key
+
+    mbutton3.config(text="Recording...")
+    root.update()
+
+    event = keyboard.read_event(suppress=True)
+
+    if event.event_type == keyboard.KEY_DOWN:
+        recorded_key = event.name
+        print(f"Key recorded: {recorded_key}")
+        mbutton3.config(text=f"Key recorded: {recorded_key}")
+        root.update()
+        return
 
 
 def exit_listener():
@@ -95,6 +112,21 @@ def autoclicker(initialise=True):
     root.update()
 
 
+def start_on():
+    global recorded_key
+
+    if not recorded_key:
+        mbutton2.config(text="No key recorded!")
+        root.update()
+        time.sleep(0.5)
+        mbutton2.config(text="Start on:")
+        root.update()
+        return
+
+    keyboard.wait(recorded_key)
+    threading.Thread(target=autoclicker, daemon=True, args=(False,)).start()
+
+
 notebook = ttk.Notebook(root)
 notebook.pack(fill="both", expand=True)
 
@@ -109,12 +141,19 @@ mlabel = ttk.Label(tab_macro, text="Macros", font=("Calibri", 20))
 mlabel.grid(row=1, column=3)
 
 mbutton = ttk.Button(tab_macro, text="Autoclicker - Stop with 'c'",
-                     command=lambda: threading.Thread(target=autoclicker(), daemon=True).start())
+                     command=lambda: threading.Thread(target=autoclicker, daemon=True).start())
 mbutton.grid(row=2, column=0)
 
 mcbox = ttk.Combobox(tab_macro, values=["10 CPS", "15 CPS", "20 CPS", "100 CPS"], state="readonly",
                      textvariable=clickspeed)
 mcbox.grid(row=2, column=1, padx=10)
+
+mbutton2 = ttk.Button(tab_macro, text="Start on:",
+                      command=lambda: threading.Thread(target=start_on, daemon=True).start())
+mbutton2.grid(row=3, column=0, pady=10)
+
+mbutton3 = ttk.Button(tab_macro, text="Record", command=lambda: threading.Thread(target=record, daemon=True).start())
+mbutton3.grid(row=3, column=1)
 
 threading.Thread(target=exit_listener, daemon=True).start()
 root.mainloop()
