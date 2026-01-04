59337aa72c60d68a56ea1eeceb3fc67d9aaae2fe
original6001 <lukas.cols@icloud.com>
2026-01-04 17:33:43 +0000

    Fixed install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Installing dependencies for Original Macro...
-pip install keyboard mouse pydirectinput
+pip install keyboard mouse pydirectinput configparser
 echo.
 echo Installation complete!
 echo Run "Original Macro.exe" to execute the program.
